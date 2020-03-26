977f3770ed0abc38a49454fdcad86599c762249d
Jane Greene <janegreenecasa@gmail.com>
2020-03-26 15:57:49 -0600

    Created and passed hash tests

--- lib/board.rb
+++ lib/board.rb
@@ -1,33 +1,25 @@
 require_relative 'cell'
 class Board
   attr_reader :cells, :coordinates
+
   def initialize
     @cells = {}
-    #@coordinates = generate_cells
-    @coordinates = ["A1", "A2", "A3","A4",
+    @coordinates = ["A1", "A2", "A3", "A4",
         "B1", "B2", "B3", "B4",
-        "C1", "C2", "C3","C4",
+        "C1", "C2", "C3", "C4",
         "D1", "D2", "D3", "D4"]
   end
 
   def generate_cells
-    # @coordinates = [:A1, :A2, :A3,:A4,
-    #   :B1, :B2, :B3, :B4,
-    #   :C1, :C2, :C3,:C4,
-    #   :D1, :D2, :D3, :D4]
     @coordinates.each do |coordinate|
       @cells[coordinate] = Cell.new(coordinate)
     end
     @cells
-    #require "pry"; binding.pry
   end
 
   def valid_coordinate?(specified_coordinate)
     @coordinates.any? do |coordinate|
       coordinate == specified_coordinate
-
     end
   end
-
-
 end
